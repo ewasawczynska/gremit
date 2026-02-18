8454fb956ea09836dbbf1c67cc0c51ce8a6908ed
ewasawczynska <sawczynska.e@gmail.com>
2026-02-18 20:36:42 +0100

    delete font links & sdk, add local font

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-// initialize element SDK
-if (window.elementSdk) {
-  window.elementSdk.init({
-    defaultConfig,
-    onConfigChange,
-    mapToCapabilities,
-    mapToEditPanelValues
-  });
-}
-
 // smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
